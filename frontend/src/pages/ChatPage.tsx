bfac0185c607cb0b965fb44e9db70248293f89b0
Matija Djuricic <98923192+MatijaDjuricic@users.noreply.github.com>
2024-04-17 18:05:20 +0200

    fix(socket): connection state

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { HubConnectionBuilder } from '@microsoft/signalr';
+import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../store/store';
 import { addMessages } from '../store/messagesSlice';
@@ -10,17 +10,16 @@ import SideBar from "../components/SideBar";
 import send_icon from '../assets/send.svg';
 import ChatCSS from './ChatPage.module.css';
 const ChatPage = () => {
+    const URL = import.meta.env.VITE_SOCKET_SERVICE_URL;
     const user = useUserContext();
     const messages = useSelector((state: RootState) => state.messages);
     const dispatch = useDispatch<AppDispatch>();
-    const URL = import.meta.env.VITE_SOCKET_SERVICE_URL;
-    const connection = new HubConnectionBuilder().withUrl(URL).build();
     const { errorMessage } = useToastMessage();
+    const [connection, setConnection] = useState<HubConnection>();
     const [message, setMessage] = useState<string>('');
     const useEffectRef = useRef<boolean>(false);
     const sendMessage = async () => {
-        await connection.start().then(() => console.log('Connected to SignalR')).catch(err => console.error('Error connecting to SignalR:', err));
-        if (message) {
+        if (connection && message) {
             await connection.invoke('SendMessage', user.id.toString(), `${user.first_name} ${user.last_name}`, message);
             dispatch(addMessages({
                 id: user.id.toString(),
@@ -31,9 +30,9 @@ const ChatPage = () => {
         }
         else errorMessage("Поље за поруку је празно");
     }
-    useEffect(() => {
-        if (useEffectRef.current == false) {
-            connection.start().then(() => console.log('Connected to SignalR')).catch(err => console.error('Error connecting to SignalR:', err));
+    const startConnecton = async () => {
+        try {
+            const connection = new HubConnectionBuilder().withUrl(URL).configureLogging(LogLevel.Information).build();
             connection.on('ReceiveMessage', (id, username, message) => {
                 if (user.id.toString() != id) {
                     dispatch(addMessages({
@@ -42,8 +41,15 @@ const ChatPage = () => {
                         user: username
                     }));
                 }
-            });
+            })
+            await connection.start();
+            setConnection(connection);
+        } catch (e) {
+            errorMessage(e ? e.toString() : '');
         }
+    }
+    useEffect(() => {
+        if (useEffectRef.current == false) startConnecton();
         return () => {
             useEffectRef.current = true;
         }
