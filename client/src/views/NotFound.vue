f503f2cc651685121e31cbce7a9dacb8101cc7c9
Matija Djuricic <98923192+MatijaDjuricic@users.noreply.github.com>
2025-03-12 13:53:01 +0100

    added not found page

--- client/src/views/NotFound.vue
+++ client/src/views/NotFound.vue
@@ -1,6 +1,8 @@
-<script lang="ts" setup>
-</script>
 <style src="./NotFound.module.css" module/>
 <template>
-    <h1>Not found page</h1>
+    <div :class="$style.container">
+        <h1 :class="$style.title">Грешка 404: Страница није пронађена</h1>
+        <p>Изгледа да сте дошли на страницу која не постоји. Молимо вас да се вратите на почетну страницу.</p>
+        <router-link to="/" :class="$style.home_link">Почетна страница</router-link>
+    </div>
 </template>
