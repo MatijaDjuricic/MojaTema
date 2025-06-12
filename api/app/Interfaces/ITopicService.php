b33f56fb9611162f965021f2a511173c4864723a
Matija Djuricic <98923192+MatijaDjuricic@users.noreply.github.com>
2025-06-13 00:09:14 +0200

    Update ITopicService.php

--- api/app/Interfaces/ITopicService.php
+++ api/app/Interfaces/ITopicService.php
@@ -3,8 +3,8 @@
 namespace App\Interfaces;
 
 use App\Http\Requests\Topic\CreateTopicRequest;
-use App\Http\Requests\Topic\updateTopicRequest;
-use App\Http\Requests\Topic\updateTopicStatusRequest;
+use App\Http\Requests\Topic\UpdateTopicRequest;
+use App\Http\Requests\Topic\UpdateTopicStatusRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 interface ITopicService {
@@ -14,7 +14,7 @@ interface ITopicService {
     public function getTopicsByProfessor(int $id): JsonResource;
     public function createTopic(CreateTopicRequest $request): JsonResource;
     public function importTopics(Request $request): bool;
-    public function updateTopic(updateTopicRequest $request, int $id): JsonResource;
-    public function updateTopicStatus(updateTopicStatusRequest $request, int $id): JsonResource;
+    public function updateTopic(UpdateTopicRequest $request, int $id): JsonResource;
+    public function updateTopicStatus(UpdateTopicStatusRequest $request, int $id): JsonResource;
     public function deleteTopic(int $id): bool;
 }
